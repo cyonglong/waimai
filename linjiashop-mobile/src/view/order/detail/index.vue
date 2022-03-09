39982607de0f67ea8b360790c633ed49948cc8dc
cyonglong <1091205968@qq.com>
2022-03-09 22:52:19 +0800

    确认取餐

--- linjiashop-mobile/src/view/order/detail/index.vue
+++ linjiashop-mobile/src/view/order/detail/index.vue
@@ -15,7 +15,7 @@
                      class="footer_btn">
                     <!-- <van-button size="small" type="default" @click="contact">联系客服</van-button> -->
                     <van-button size="small" type="danger" @click="payment" v-show="order.statusName=='待付款'">立即付款</van-button>
-                    <van-button size="small" type="danger" @click="confirmReceive" v-show="order.statusName=='已发货'">确认收货</van-button>
+                    <van-button size="small" type="danger" @click="confirmReceive" v-show="order.statusName=='已发货'">确认取餐</van-button>
                 </div>
             </van-panel>
             <van-panel :title="'订单编号: ' + order.orderSn">
